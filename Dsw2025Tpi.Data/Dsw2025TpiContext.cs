bf7b3bc75be2e476ca6911be018101c4c977bcff
MastafaNazar <jeremiasmastafa72@gmail.com>
2025-06-25 11:31:30 -0300

    Agregar configuraciones de la DB a capa Data

--- Dsw2025Tpi.Data/Dsw2025TpiContext.cs
+++ Dsw2025Tpi.Data/Dsw2025TpiContext.cs
@@ -1,7 +1,30 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Dsw2025Tpi.Data.Configurations;
+using Dsw2025Tpi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dsw2025Tpi.Data;
 
 public class Dsw2025TpiContext: DbContext
 {
+    public Dsw2025TpiContext(DbContextOptions<Dsw2025TpiContext> options) : base(options)
+    {
+    }
+
+    // Tablas
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Configure your entities here
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfiguration(new OrderConfiguration());
+        modelBuilder.ApplyConfiguration(new OrderItemConfiguration());
+        modelBuilder.ApplyConfiguration(new ProductConfiguration());
+    }
+    // DbSet properties for your entities
+    // public DbSet<YourEntity> YourEntities { get; set; }
+
 }
