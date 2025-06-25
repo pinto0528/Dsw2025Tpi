0bd72464786793de2e37933f4d9d8c05d4363b18
MastafaNazar <jeremiasmastafa72@gmail.com>
2025-06-25 12:41:13 -0300

    Modificar configuraciones de entidades en capa Data

--- Dsw2025Tpi.Data/Configurations/OrderConfiguration.cs
+++ Dsw2025Tpi.Data/Configurations/OrderConfiguration.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Dsw2025Tpi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,34 @@ namespace Dsw2025Tpi.Data.Configurations
 {
     public class OrderConfiguration : IEntityTypeConfiguration<Order>
     {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.HasKey(o => o.Id);
 
+            builder.Property(o => o.Date)
+                .IsRequired();
+
+            builder.Property(o => o.ShippingAddress).IsRequired()
+                .HasMaxLength(500);
+
+            builder.Property(o => o.BillingAddress).IsRequired()
+                .HasMaxLength(500);
+
+            builder.Property(o => o.Status)
+                .IsRequired();
+
+            builder.Property(o => o.TotalAmount)
+                .IsRequired()
+                .HasColumnType("decimal(18,2)");
+
+            builder.HasMany(o => o.OrderItems)
+                .WithOne()
+                .HasForeignKey(oi => oi.OrderId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Property(o => o.CustomerId).IsRequired();
+
+
+        }
     }
 }
--- Dsw2025Tpi.Data/Configurations/ProductConfiguration.cs
+++ Dsw2025Tpi.Data/Configurations/ProductConfiguration.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Dsw2025Tpi.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,36 @@ namespace Dsw2025Tpi.Data.Configurations
 {
     public class ProductConfiguration : IEntityTypeConfiguration<Product>
     {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(p => p.Id);
 
+            builder.HasIndex(p => p.Sku)
+                .IsUnique();
+
+            builder.Property(p => p.Sku).HasMaxLength(50);
+
+            builder.Property(p => p.InternalCode)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(p => p.Description)
+                .HasMaxLength(500);
+
+            builder.Property(p => p.CurrentUnitPrice)
+                .IsRequired()
+                .HasColumnType("decimal(18,2)");
+
+            builder.Property(p => p.StockQuantity)
+                .IsRequired();
+
+            builder.Property(p => p.IsActive)
+                .IsRequired();
+
+        }
     }
 }
