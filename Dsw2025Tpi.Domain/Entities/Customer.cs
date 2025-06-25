d56c9ce6a0a1074e1c7fc3e5c45109d3aaa4e37a
MastafaNazar <jeremiasmastafa72@gmail.com>
2025-06-25 10:47:10 -0300

    Agregar entidades y enum

---
+++ Dsw2025Tpi.Domain/Entities/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Tpi.Domain.Entities
+{
+    public class Customer : EntityBase
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+}
