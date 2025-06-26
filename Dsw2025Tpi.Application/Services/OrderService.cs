b8ec1932f3ff59bd109d14680dd0c06b1627a61c
MastafaNazar <Jeremias.MastafaNazar@alu.frt.utn.edu.ar>
2025-06-26 00:23:29 -0300

    Definir servicios en capa Application

---
+++ Dsw2025Tpi.Application/Services/OrderService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dsw2025Tpi.Domain.Interfaces;
+
+namespace Dsw2025Tpi.Application.Services
+{
+    public class OrderService
+    {
+        private readonly IRepository _orderRepository;
+        public OrderService(IRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+    }
+}
---
+++ Dsw2025Tpi.Application/Services/ProductService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dsw2025Tpi.Domain.Interfaces;
+
+namespace Dsw2025Tpi.Application.Services
+{
+    public class ProductService
+    {
+        private readonly IRepository _productRepository;
+        public ProductService(IRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+    }
+}
