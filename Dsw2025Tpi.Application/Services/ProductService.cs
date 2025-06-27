d7a385e6d694d1d48cafce4e7a57a03c1764b511
MastafaNazar <Jeremias.MastafaNazar@alu.frt.utn.edu.ar>
2025-06-27 13:32:16 -0300

    Agregar validaciones

--- Dsw2025Tpi.Application/Services/ProductService.cs
+++ Dsw2025Tpi.Application/Services/ProductService.cs
@@ -25,7 +25,23 @@ namespace Dsw2025Tpi.Application.Services
         }
 
         public async Task<ProductModel.ProductResponse> Add(ProductModel.ProductRequest request) 
-        { 
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException(nameof(request), "El request no puede ser nulo.");
+            }
+
+            // Validacion de campos requeridos
+            if (request.CurrentUnitPrice <= 0)
+            {
+                throw new ArgumentException("El precio unitario debe ser mayor a cero.");
+            }
+
+            if(request.StockQuantity < 0)
+            {
+                throw new ArgumentException("La cantidad de stock no puede ser negativa.");
+            }
+
             var productEntity = request.ToEntity();
             var savedProductEntity = await _productRepository.Add(productEntity); 
             return _entityMapper.ToResponse(savedProductEntity);
@@ -33,9 +49,25 @@ namespace Dsw2025Tpi.Application.Services
 
         public async Task<ProductModel.ProductResponse> Update(Guid id, ProductModel.ProductRequest request)
         {
+            if (request == null)
+            {
+                throw new ArgumentNullException(nameof(request), "El request no puede ser nulo.");
+            }
+
             // Validacion de existencia del producto
             var existingProduct = await _productRepository.GetById<Product>(id) ??
                 throw new KeyNotFoundException($"El producto de ID {id} no fue encontrado.");
+
+            // Validacion de campos requeridos
+            if (request.CurrentUnitPrice <= 0)
+            {
+                throw new ArgumentException("No se puede actualizar. El precio unitario debe ser mayor a cero.");
+            }
+
+            if (request.StockQuantity < 0)
+            {
+                throw new ArgumentException("No se puede actualizar. La cantidad de stock no puede ser negativa.");
+            }
 
             // Actualizacion de campos
             existingProduct.Sku = request.Sku;
